15c45c7fe9328fffca64e39c122089ec5dec2ec4
Nshiman <naoya.nagashima@gmail.com>
2021-01-20 22:05:00 +0900

    develop 20210120

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,7 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
-  },
-  {
-    Comp: GitHub,
-    alt: 'github icon',
-    link: 'https://github.com/ijjk',
-  },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  },
-  {
-    Comp: Envelope,
-    alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'https://twitter.com/eveilpaint',
   },
 ]
 
@@ -43,8 +28,8 @@ export default () => (
       <h1 style={{ marginTop: 0 }}>Contact</h1>
 
       <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
+        Eveil - Illustrator @{' '}
+        <ExtLink href="https://comet-stone.com">Comet Stone.</ExtLink>
       </div>
 
       <div className={contactStyles.links}>
