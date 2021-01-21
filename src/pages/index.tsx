af8c1ddbe248b712e2a16633f4087c7c9d83ae3a
Nshiman <naoya.nagashima@gmail.com>
2021-01-21 15:52:53 +0900

    background change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 import Features from '../components/features'
 import GitHub from '../components/svgs/github'
-//import sharedStyles from '../styles/shared.module.css'
+import sharedStyles from '../styles/shared.module.css'
 
 export default () => (
   <>
